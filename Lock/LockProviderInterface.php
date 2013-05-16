f08456de9df19bd5f8364d9f0488d560e4f8de56
Toni Uebernickel <tuebernickel@gmail.com>
2013-05-16 16:54:04 +0200

    add LockProviderInterface
    This interface may be used by repositories when acquiring a new Lock.

---
+++ Lock/LockProviderInterface.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Havvg\Component\Lock\Lock;
+
+use Havvg\Component\Lock\Acquirer\AcquirerInterface;
+use Havvg\Component\Lock\Resource\ResourceInterface;
+
+interface LockProviderInterface
+{
+    /**
+     * Create a new Lock for the given acquirer and resource.
+     *
+     * The Lock is created, is does not verify its validity!
+     *
+     * @param AcquirerInterface $acquirer
+     * @param ResourceInterface $resource
+     *
+     * @return LockInterface
+     */
+    public function create(AcquirerInterface $acquirer, ResourceInterface $resource);
+}
